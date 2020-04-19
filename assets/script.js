17f739d74d2df9f8afacc725c9a4fbfad2eeecfc
Austin Cudworth <austincudworth@gmail.com>
2020-04-19 14:43:33 -0700

    Add form submitting capability

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,12 @@ function appendChildren(element, children){
     children.forEach(child => element.appendChild(child));
 }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book() {
+    this.title;
+    this.author;
+    this.pages;
+    this.read
+    ;
     this.info = function(){
         if (this.read) {
             const read = 'has been read'
@@ -25,9 +26,7 @@ Book.prototype.update = function (title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 
-    display.render();
 }
-
 
 function removeFromLibrary(index) {
     myLibrary.splice(index, 1);
@@ -46,7 +45,7 @@ function toggleReadStatus(index) {
 }
 
 function Display(){
-    this.node;
+    this.node = document.createElement('div');
     this.library;
 }
 
@@ -58,7 +57,7 @@ Display.prototype.render = function () {
     new_book_button.textContent = 'NEW BOOK'
     new_book_button.addEventListener('click', e => form.render());
 
-    this.node.appendChild(new_book_button);
+    node.appendChild(new_book_button);
     
     this.library.forEach(function(e, index){
         div = document.createElement('div');
@@ -85,61 +84,92 @@ Display.prototype.render = function () {
 
 
 function Form(){
-    this.node;
+    this.node = document.createElement('form');
 
-}
-
-Form.prototype.render = function(){
-    node = this.node;
-    node.innerHTML = '';
-
-    const fields = {
+    this.fields = {
         title: 'text',
         author: 'text',
         pages: 'text',
         read: 'checkbox'
     };
+}
+
+Form.prototype.render = function(){
+    
+    const node = this.node;
+    const fields = this.fields;
+    node.innerHTML = '';
+
+    const ul  = document.createElement('ul');
+    node.appendChild(ul);
 
     Object.keys(fields).forEach(function(key) {
 
-        const div = document.createElement('div');
-        node.appendChild(div);
+        const li = document.createElement('li');
+        ul.appendChild(li);
 
         const label = document.createElement('label');
-        div.appendChild(label);
+        li.appendChild(label);
 
         const input = document.createElement('input');
-        div.appendChild(input);
+        li.appendChild(input);
+
+        const id = `form_${key}`;
 
         label.textContent = `${key}: `;
-        label.setAttribute('for', key);
+        label.setAttribute('for', id);
 
+        input.id = id;
         input.name = key;
         input.type = fields[key];
     });
 
-    const submit_button = document.createElement('button');
-    submit_button.textContent = 'SUBMIT';
-    node.appendChild(submit_button);
+    const button = document.createElement('button');
+    button.textContent = 'SUBMIT';
+    button.type = 'button';
+    button.addEventListener('click', e => this.read());
+    node.appendChild(button);
     
 }
 
+Form.prototype.read = function(){
+    const fields = this.fields;
+
+    const book = new Book();
+    myLibrary.unshift(book);
+
+    Object.keys(fields).forEach(function(key){
+        const input = document.getElementById(`form_${key}`);
+        if (fields[key] == 'text'){
+            book[key] = input.value;
+        } else {
+            book[key] = input.checked;
+        };
+    });
+    
+    this.node.innerHTML = '';
+    display.render();
+}
+
+
 let myLibrary = [];
 
-myLibrary.push(new Book('The Lord of the Rings', 'J.R.R. Tolkien', 40000, true));
-myLibrary.push(new Book('For Whom the Bell Tolls','Ernest Hemingway', 12, false));
-myLibrary.push(new Book('A River Runs Through It','Norman Maclean', 200, true));
+const book1 = new Book();
+const book2 = new Book();
+book1.update('The Lord of the Rings', 'J.R.R. Tolkien', 40000, true);
+book2.update('For Whom the Bell Tolls','Ernest Hemingway', 12, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
 
+console.log(myLibrary);
 
 form = new Form;
-form.node = document.createElement('form');
 display = new Display;
-display.node = document.createElement('div');
 
 const body = document.querySelector('body')
+
 body.appendChild(form.node);
 body.appendChild(display.node);
 
 display.library = myLibrary;
-
 display.render();
